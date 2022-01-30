c69c5b35aa7a9b647ceec8d7e3e243a97903c8ae
t-houssian <tylerhoussian@gmail.com>
2022-01-29 20:38:37 -0700

    Added Code for prove 8

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const professionalRoutes = require('./routes/professional');
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/professional', professionalRoutes);
+
+app.listen(8080);
